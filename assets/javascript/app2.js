e7f560f76afe5e55c530982617be6078b349e731
kaetee425 <kingpang@outlook.com>
2017-09-12 22:11:47 -0700

    i give up

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -28,27 +28,70 @@ $(document).ready(function(){
 	run();
 
 	// quiz
+
+	var quizBox = $("#quiz")
+	var submit = $("#submit")
+	var results = $("#results")
+
+
+	var questions = [
+		{
+			question: "1. How many Pokemon can Eevee evolve into?",
+			choices: ['a. 6', 'b. 7', 'c. 8', 'd. 9'],
+			answer: "b. 7"
+		},
+		{
+			question: "2. Which Pokemon does Pikachu evolve into?",
+			choices: ['a. Pichu', 'Raichu', 'Does Not Evole', 'Pachirisu'],
+			answer: "b. Raichu"
+		},
+		{
+			question: "3. What is Ash's full name?",
+			choices: ['a. Ash Ketchum', 'b. Ash Ketchup', 'c. Ash Cetchum', 'Ad. sh Catch-em'],
+			answer: "a. Ash Ketchum"
+		},
+		{
+			question: "4. Who constantly tries to steal Pokemon?",
+			choices: ['a. Team Rocket', 'b. Team Magma', 'c. Team Aqua', 'd. Team Skull'],
+			answer: "a. Team Rocket"
+		},
+		{
+			question: "5. What is the legend of where Pokemon come from?",
+			choices: ['a. Earth', 'b. Space', 'c. Ocean', 'd. Volcano'],
+			answer: "b. Space"
+		},
+		{
+			question: "6. What is round and cute but will sing you to sleep?",
+			choices: ['a. Jiggypuff', 'b. Celfairy', 'c. Chansey', 'd. Clefable'],
+			answer: "a. Jiggypuff"
+		},
+		
+	];
+
 	function quizTime(){
 
 		var output = [];
+		
 
-		questions.forEach(function(currentQuest, questNum){
-			var answers =[];
+		for (var i=0; i < questions.length; i++){
+			var answers = [];
 
-			for (choices in currentQuest.answers){
+			for(letter in questions[i].answers){
 				answers.push(
-		          `<label>
-		            <input type="radio" name="question${questNum}" value="${choices}">
-		            ${choices} :
-		            ${currentQuest.answers[choices]}
-		          </label>`
-		      	);
+					'<label>'
+						+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+						+ letter + ': '
+						+ questions[i].answers[letter]
+					+ '</label>'
+				);
 			}
-			output.push(
-		        `<div class="question"> ${currentQuest.question} </div>
-		        <div class="answers"> ${answers.join('')} </div>`
-     		 );
-		});
+
+				output.push(
+					'<div class="question">' + questions[i].question + '</div>'
+					+ '<div class="answers">' + answers.join('') + '</div>'
+				);
+
+		}
 
 		console.log(output)
 
@@ -80,44 +123,6 @@ $(document).ready(function(){
 		results.innerHTML = numCorrect + " out of " + questions.length
 	};
 
-	var quizBox = $("#quiz")
-	var submit = $("#submit")
-	var results = $("#results")
-
-
-	var questions = [
-		{
-			question1: "1. How many Pokemon can Eevee evolve into?",
-			choices: ['a. 6', 'b. 7', 'c. 8', 'd. 9'],
-			answer: "b. 7"
-		},
-		{
-			question2: "2. Which Pokemon does Pikachu evolve into?",
-			choices: ['a. Pichu', 'Raichu', 'Does Not Evole', 'Pachirisu'],
-			answer: "b. Raichu"
-		},
-		{
-			question3: "3. What is Ash's full name?",
-			choices: ['a. Ash Ketchum', 'b. Ash Ketchup', 'c. Ash Cetchum', 'Ad. sh Catch-em'],
-			answer: "a. Ash Ketchum"
-		},
-		{
-			question4: "4. Who constantly tries to steal Pokemon?",
-			choices: ['a. Team Rocket', 'b. Team Magma', 'c. Team Aqua', 'd. Team Skull'],
-			answer: "a. Team Rocket"
-		},
-		{
-			question5: "5. What is the legend of where Pokemon come from?",
-			choices: ['a. Earth', 'b. Space', 'c. Ocean', 'd. Volcano'],
-			answer: "b. Space"
-		},
-		{
-			question6: "6. What is round and cute but will sing you to sleep?",
-			choices: ['a. Jiggypuff', 'b. Celfairy', 'c. Chansey', 'd. Clefable'],
-			answer: "a. Jiggypuff"
-		},
-		
-	];
 
 	quizTime();
 
